f134ac3f43c53a42947f730b724b28759ca8e665
tangdaye <tangdaye1996@hotmail.com>
2019-01-24 01:05:39 +0800

    java mail

--- StoryMappingTool-Server/src/test/java/nju/software/service/EmailServiceTest.java
+++ StoryMappingTool-Server/src/test/java/nju/software/service/EmailServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 
 /**
@@ -23,6 +25,12 @@ public class EmailServiceTest {
 
     @Test
     public void test1() {
+        Properties prop = System.getProperties();
+        System.getProperties().put("proxySet","true");
+        // 设置http访问要使用的代理服务器的地址
+        prop.setProperty("http.proxyHost","219.219.113.223");
+        // 设置http访问要使用的代理服务器的端口
+        prop.setProperty("http.proxyPort","81");
         String address = "tangdaye1996@hotmail.com";
         String content = "ABCD";
         String title = "Story Mapping Tool的验证码";
