4e8b122665836cbd5c61b9a4dd5d7a7528005600
tangdaye <tangdaye1996@hotmail.com>
2019-01-24 01:31:09 +0800

    java mail

--- StoryMappingTool-Server/src/test/java/nju/software/util/EmailUtilTest.java
+++ StoryMappingTool-Server/src/test/java/nju/software/util/EmailUtilTest.java
@@ -14,15 +14,15 @@ import static org.junit.Assert.assertTrue;
 public class EmailUtilTest {
     @Test
     public void test1() {
-        Properties prop = System.getProperties();
-        System.getProperties().put("proxySet","true");
-        // 设置http访问要使用的代理服务器的地址
-        prop.setProperty("http.proxyHost","219.219.113.223");
-        // 设置http访问要使用的代理服务器的端口
-        prop.setProperty("http.proxyPort","81");
-        String address = "tangdaye1996@hotmail.com";
-        String content = "ABCD";
-        String title = "Story Mapping Tool的验证码";
-        assertTrue(EmailUtil.sendEmail(address, title, content));
+//        Properties prop = System.getProperties();
+//        System.getProperties().put("proxySet","true");
+//        // 设置http访问要使用的代理服务器的地址
+//        prop.setProperty("http.proxyHost","219.219.113.223");
+//        // 设置http访问要使用的代理服务器的端口
+//        prop.setProperty("http.proxyPort","81");
+//        String address = "tangdaye1996@hotmail.com";
+//        String content = "ABCD";
+//        String title = "Story Mapping Tool的验证码";
+//        assertTrue(EmailUtil.sendEmail(address, title, content));
     }
 }
