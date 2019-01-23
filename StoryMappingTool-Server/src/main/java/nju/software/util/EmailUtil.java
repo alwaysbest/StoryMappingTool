5ca617fb6fd37a95cd1547be123109e020cd93e7
tangdaye <tangdaye1996@hotmail.com>
2019-01-24 01:27:22 +0800

    java mail

--- StoryMappingTool-Server/src/main/java/nju/software/util/EmailUtil.java
+++ StoryMappingTool-Server/src/main/java/nju/software/util/EmailUtil.java
@@ -1,10 +1,14 @@
 package nju.software.util;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-import javax.annotation.Resource;
-
+import com.sun.mail.util.MailSSLSocketFactory;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.util.Date;
+import java.util.Properties;
 
 /**
  * Time       : 2019/1/9 9:49 PM
@@ -13,4 +17,41 @@ import javax.annotation.Resource;
  */
 public class EmailUtil {
 
+    public static boolean sendEmail(String address, String title, String content) {
+        Properties props = new Properties();
+        props.setProperty("mail.debug", "true");
+        props.setProperty("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", 465);
+        props.setProperty("proxySet", "true");
+        props.setProperty("socksProxyHost", "219.219.113.223");
+        props.setProperty("socksProxyPort", "81");
+        String host = "smtp.qq.com";
+        props.setProperty("mail.smtp.host", host);
+        props.setProperty("mail.transport.protocol", "smtp");
+        try {
+            MailSSLSocketFactory sf = new MailSSLSocketFactory();
+            sf.setTrustAllHosts(true);
+            props.put("mail.smtp.ssl.enable", "true");
+            props.put("mail.smtp.ssl.socketFactory", sf);
+            Session session = Session.getInstance(props);
+            Message msg = new MimeMessage(session);
+            msg.setSubject(title);
+            msg.setSentDate(new Date());
+            msg.setText("\n验证码: " + content);
+            String account = "1138833148@qq.com";
+            msg.setFrom(new InternetAddress(account, "Story Mapping Tool", "UTF-8"));
+            Transport transport = session.getTransport();
+            String password = "erhfoiapfzgqgbdd";
+            transport.connect(host, account, password);
+            transport.sendMessage(msg, new Address[]{new InternetAddress(address)});
+            transport.close();
+        } catch (GeneralSecurityException | MessagingException | UnsupportedEncodingException e) {
+            return false;
+//            e.printStackTrace();
+        }
+        return true;
+
+
+    }
+}
 }
